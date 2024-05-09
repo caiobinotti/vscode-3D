4fcfce40be523665f29d5bf1d5d61115f4a3eb73
caiobinotti <130671116+caiobinotti@users.noreply.github.com>
2024-05-09 07:42:00 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 const botoes = document.querySelectorAll(".botao");
+const texto = document.querySelectorAll("aba-conteudo");
+const contadores = document.querySelectorAll(".contadore");
+const tempoobjetivo1 = new Date ("2024-05-10T00;00;00");
 
-for (let i=0; i< botoes.length;i++){
+contadores[0].textContent = tempoobjetivo1; 
+
+for (let i=0; i<botoes.length;i++){
     botoes[i].onclick=function(){
+        for(j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
         botoes[i].classList.add("ativo");
+        texto[i].classList.add("ativo");
     }
 }
