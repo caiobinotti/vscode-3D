bb8030e5518edcf4a88c7129cfd4539cf1baf236
caiobinotti <130671116+caiobinotti@users.noreply.github.com>
2024-08-30 07:43:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPergunta = document.querySelector('.caixa-pergunta');
+const caixaAlternativa = document.querySelector('.caixa-alternativa');
+const caixaResultado = document.querySelector('.caixa-resultado');
+
+const perguntas = [
+    {
+        enunciado: "você gosta da ideia da interligencia artificial?",
+        alternativas:["sim", "nao"]
+    },
+    {
+        enunciado: " a inteligencia artificial pode desenvolver conciência própria?",
+        alternativas:["sim", "nao"]
+    },
+    {
+        enunciado: "a inteligencia artificial pode tomar decisoes eticas de forma completamente autonoma?",
+        alternativas:["sim", "nao"]
+    }
+]
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    perguntaAtual = perguntas[posicao];
+    caixaPergunta.textContent = perguntaAtual.enunciado;
+}
+
+
+
+
