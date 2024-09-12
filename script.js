d921055a54f359832ea9860aa6c3841962768035
caiobinotti <130671116+caiobinotti@users.noreply.github.com>
2024-09-12 08:32:40 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -24,6 +24,15 @@ let perguntaAtual;
 function mostraPergunta(){
     perguntaAtual = perguntas[posicao];
     caixaPergunta.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativa){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativa.appendChild(botaoAlternativas);
+
+    }
 }
 
 
